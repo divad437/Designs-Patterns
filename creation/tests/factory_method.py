196df0e306b258ffd4dc887643ce6862e3817e75
divad <132532789+divad437@users.noreply.github.com>
2024-02-10 09:30:33 +0100

    adding factory_method test file

---
+++ creation/tests/factory_method.py
@@ -0,0 +1,33 @@
+# import pytest
+
+from creation.fabrique import Car, CarFactory, Motorcycle, MotorcycleFactory
+
+
+class TestCodeUnderTest:
+    def test_car_creation(self) -> None:
+        car_factory = CarFactory()
+        car = car_factory.create_vehicle()
+        assert isinstance(car, Car)
+
+    def test_motorcycle_creation(self) -> None:
+        motorcycle_factory = MotorcycleFactory()
+        motorcycle = motorcycle_factory.create_vehicle()
+        assert isinstance(motorcycle, Motorcycle)
+
+    # def test_car_drive(self) -> None:
+    #     car_factory = CarFactory()
+    #     car = car_factory.create_vehicle()
+    #     assert car.drive() == "The car drives on the road"
+
+    # def test_vehicle_instantiation(self) -> None:
+    #     with pytest.raises(TypeError):
+    #         Vehicle()
+
+    # def test_vehicle_factory_instantiation(self) -> None:
+    #     with pytest.raises(TypeError):
+    #         VehicleFactory()
+
+    # def test_car_factory_create(self) -> None:
+    #     car_factory = CarFactory()
+    #     car = car_factory.create_vehicle()
+    #     assert isinstance(car, Car)
