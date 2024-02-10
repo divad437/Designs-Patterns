2d4dae3e55454d249302d3424b2891413a0f930d
divad <132532789+divad437@users.noreply.github.com>
2024-02-10 09:40:59 +0100

    rename factory_method test file

--- creation/tests/test_factory_method.py
+++ creation/tests/test_factory_method.py
@@ -1,5 +1,3 @@
-# import pytest
-
 from creation.fabrique import Car, CarFactory, Motorcycle, MotorcycleFactory
 
 
@@ -9,25 +7,17 @@ class TestCodeUnderTest:
         car = car_factory.create_vehicle()
         assert isinstance(car, Car)
 
-    def test_motorcycle_creation(self) -> None:
+    def test_motorcycle_creation(self):
         motorcycle_factory = MotorcycleFactory()
         motorcycle = motorcycle_factory.create_vehicle()
         assert isinstance(motorcycle, Motorcycle)
 
-    def test_car_drive(self) -> None:
-        car_factory = CarFactory(self)
-        car = car_factory.create_vehicle(self)
+    def test_car_drive(self):
+        car_factory = CarFactory()
+        car = car_factory.create_vehicle()
         assert car.drive() == "The car drives on the road"
 
-    # def test_vehicle_instantiation() -> None:
-    #     with pytest.raises(TypeError):
-    #         Vehicle()
-
-    # def test_vehicle_factory_instantiation() -> None:
-    #     with pytest.raises(TypeError):
-    #         VehicleFactory()
-
-    def test_car_factory_create(self) -> None:
+    def test_car_factory_create(self):
         car_factory = CarFactory()
         car = car_factory.create_vehicle()
         assert isinstance(car, Car)
