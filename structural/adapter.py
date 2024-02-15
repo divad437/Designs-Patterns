8236ac94adc99245b8978e335442e848746dc0bd
divad <132532789+divad437@users.noreply.github.com>
2024-02-16 00:09:11 +0100

    adding typing

--- structural/adapter.py
+++ structural/adapter.py
@@ -1,14 +1,3 @@
-class RoundHole:
-    def __init__(self, radius):
-        self.__radius = radius
-
-    def get_radius(self) -> int:
-        return self.__radius
-
-    def fits(self, peg) -> bool:
-        return self.get_radius() >= peg.get_radius()
-
-
 class RoundPeg:
     def __init__(self, radius):
         self.__radius = radius
@@ -17,8 +6,19 @@ class RoundPeg:
         return self.__radius
 
 
+class RoundHole:
+    def __init__(self, radius: int) -> None:
+        self.__radius = radius
+
+    def get_radius(self) -> int:
+        return self.__radius
+
+    def fits(self, peg: RoundPeg) -> bool:
+        return self.get_radius() >= peg.get_radius()
+
+
 class SquarePeg:
-    def __init__(self, width):
+    def __init__(self, width: int) -> None:
         self.__width = width
 
     def get_width(self) -> int:
@@ -26,7 +26,7 @@ class SquarePeg:
 
 
 class SquarePegAdapter:
-    def __init__(self, peg):
+    def __init__(self, peg: SquarePeg) -> None:
         self.__peg = peg
 
     def get_radius(self) -> int:
@@ -34,20 +34,22 @@ class SquarePegAdapter:
 
 
 class Application:
-    def main(self):
+    def main(self) -> None:
         round_hole = RoundHole(5)
         round_peg = RoundPeg(5)
         if round_hole.fits(round_peg):
             print("Round peg with radius 5 fits round hole with radius 5.")
         square_peg = SquarePeg(5)
         square_peg_adapter = SquarePegAdapter(square_peg)
-        square_peg_big_adapter = SquarePegAdapter(SquarePeg(20))
+        square_peg_big_adapter = SquarePegAdapter(SquarePeg(5))
         if not round_hole.fits(square_peg_big_adapter):
             print("Square peg with width 10 not fits round hole with radius 5.")
+        else:
+            print("Square peg with width 10 fits round hole with radius 5.")
         if round_hole.fits(square_peg_adapter):
             print("Square peg with width 5 fits round hole with radius 5.")
-        return 0
 
 
-app = Application()
-app.main()
+if __name__ == "__main__":
+    app = Application()
+    app.main()
